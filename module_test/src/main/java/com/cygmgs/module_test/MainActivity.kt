3366cd1e805bb50738b55fa57948187b19bed039
林映栋 <18318166399@163.com>
2020-08-10 09:37:41 +0800

    feat ：K线图框架导入

---
+++ module_test/src/main/java/com/cygmgs/module_test/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.cygmgs.module_test
+
+import android.app.Activity
+import android.os.Bundle
+import om.cygmgs.module_test.R
+
+class MainActivity : Activity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.layout_test)
+    }
+
+}
